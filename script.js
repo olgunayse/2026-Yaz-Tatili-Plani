342d6f33477c39074f5ac87f7efde8cbdf8e5a9f
olgunayse <olgunayse01@gmail.com>
2026-05-14 21:42:54 -0500

    yes

---
+++ script.js
@@ -0,0 +1,9 @@
+function showReminder() {
+  document.getElementById("reminder-text").textContent =
+    "Güneş kremi, şarj aleti, plaj havlusu ve atıştırmalıkları unutma!";
+}
+
+function showGas() {
+  document.getElementById("gas-text").textContent =
+    "Benzin fiyatları değişebilir, bu yüzden biraz ekstra bütçe ayırmak iyi olur.";
+}
